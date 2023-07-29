01837729f59a9864514cd8864ebc6491a0d810a3
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-07-29 16:59:56 +0500

    added enter raffle functionality

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { contractAddresses, abi } from "@/constants";
+import { ethers } from "ethers";
 import { useMoralis, useWeb3Contract } from "react-moralis";
+import { useNotification } from "web3uikit";
 
 const LotteryEntrance = () => {
+  const [entranceFee, setEntranceFee] = useState();
+  const [formattedEntranceFee, setFormattedEntranceFee] = useState();
   const { chainId: HexchainId, isWeb3Enabled } = useMoralis();
   const chainId = parseInt(HexchainId);
-  console.log(chainId);
+
+  const dispatch = useNotification();
+
   const raffleAddress =
     chainId in contractAddresses ? contractAddresses[chainId][0] : null;
-  console.log(raffleAddress);
+
   const { runContractFunction: getEntranceFee } = useWeb3Contract({
     abi: abi,
     contractAddress: raffleAddress, // specify the networkId
@@ -16,18 +22,68 @@ const LotteryEntrance = () => {
     params: {},
   });
 
+  const { runContractFunction: enterRaffle } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress, // specify the networkId
+    functionName: "enterRaffle",
+    params: {},
+    msgValue: entranceFee,
+  });
+
   useEffect(() => {
     const getRaffleFee = async () => {
       if (isWeb3Enabled) {
-        console.log("point 1");
         const raffleFee = await getEntranceFee();
-        console.log(raffleFee);
+        setEntranceFee(raffleFee);
+        setFormattedEntranceFee(ethers.utils.formatEther(raffleFee));
       }
     };
     getRaffleFee();
   }, [isWeb3Enabled]);
 
-  return <div></div>;
+  //THE tx WILL COME AS A PARAMETER FROM WHERE ITS CALLED
+  const handleSuccess = async (tx) => {
+    await tx.wait(1);
+    handleNewNotifiacation();
+  };
+
+  const handleNewNotifiacation = () => {
+    dispatch({
+      type: "success",
+      message: "Transaction complete!!!",
+      title: "Transaction Notification",
+      position: "topR",
+      isClosing: true,
+    });
+  };
+
+  return (
+    <div>
+      {raffleAddress ? (
+        <h4>
+          <button
+            onClick={async () => {
+              /*onSuccess just checks if the transaction
+              was sent from metamask to the chain it doesnt
+              checks for the block confirmations*/
+              await enterRaffle({
+                onSuccess: handleSuccess,
+                onError: (error) => console.log(error),
+              });
+              console.log("Raffle Entered");
+            }}
+            className="px-4 py-2 border-2"
+          >
+            Enter Raffle
+          </button>
+          Entrance Fee: {formattedEntranceFee}
+          <span className="font-semibold">Eth</span>{" "}
+        </h4>
+      ) : (
+        <div>No Raffle Address detected</div>
+      )}
+    </div>
+  );
 };
 
 export default LotteryEntrance;
